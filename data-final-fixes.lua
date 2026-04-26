f3c29dbed4018903c5d7f847bbbcab6ac6bc8bb2
Telkine2018 <Telkine2018@gmail.com>
2026-04-26 22:26:03 +0200

    Change  technology for Py mods

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -7,3 +7,15 @@ if mods["nullius"] then
 	data.raw["item"][commons.device_name].subgroup = data.raw["item"]["train-stop"].subgroup
 	table.insert(data.raw.technology["nullius-broadcasting-1"].prerequisites, name)
 end
+
+if mods["pycoalprocessing"] then
+	local tech = data.raw["technology"]["yaltn-device"]
+	tech.unit = {
+		count = 100,
+		ingredients = {
+			{ 'automation-science-pack', 1 },
+		},
+		time = 15
+	}
+	data:extend { tech }
+end
